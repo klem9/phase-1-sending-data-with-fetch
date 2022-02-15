dec219cca89412277cf2d6833d4c96f7c595e03f
Klemense <klemenseloh@gmail.com>
2022-02-15 15:18:51 -0500

    commit tree 3d58684f96d5b16a8ce689f20e6f460e3049ac8a

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData(name,email){
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          name: name,
+          email: email
+        }),
+      })
+    .then(resp => resp.json())
+    .then(data => {
+        let body = document.querySelector('body')
+        let h1 = document.querySelector('h1')
+        h1.innerHTML= `${data.id}`;
+        body.appendChild(h1);
+    }).catch(error => {
+        document.body.innerHTML = error.message
+    })
+}
+
+submitData()
